af13eeb8f503850c5228c389fae7a1cca4db5b4f
Kasper Jacobsen <k@mackwerk.dk>
2014-09-23 13:31:23 +0200

    Use argparse

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -1,14 +1,17 @@
+import argparse
 import sys
 
 from isort import isort
 
 
 def main(argv=None):
-    print sys.argv
-    argv = argv if argv is not None else sys.argv[1:]
-    print argv
-
-    # isort.SortImports('./', show_diff=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to run')
+    args = parser.parse_args(argv)
+    print args
+    # argv = argv if argv is not None else sys.argv[1:]
+    # for filename in argv:
+    #     isort.SortImports(filename, show_diff=True)
     return 0
 
 if __name__ == '__main__':
