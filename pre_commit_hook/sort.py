1305894e7518d9d3c7e7a4b9f7e0c84a6d5c4da6
Kasper Jacobsen <k@mackwerk.dk>
2014-09-23 14:05:10 +0200

    Readability

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -7,19 +7,19 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to run')
     parser.add_argument('--diff-only', action='store_true', dest='diff_only')
-    parser.add_argument('--dont-check', action='store_false', dest='check')
     parser.set_defaults(diff_only=False)
-    parser.set_defaults(check=True)
     args = parser.parse_args(argv)
 
     result = []
 
     for filename in args.filenames:
-        isort.SortImports(filename, show_diff=args.diff_only)
-        if args.check:
-            f = isort.SortImports(filename, check=args.check)
-            if f.incorrectly_sorted is True:
+        if args.diff_only:
+            isort.SortImports(filename, show_diff=True)
+            output = isort.SortImports(filename, check=True)
+            if output.incorrectly_sorted is True:
                 result.append(filename)
+        else:
+            isort.SortImports(filename)
     return len(result)
 
 if __name__ == '__main__':
