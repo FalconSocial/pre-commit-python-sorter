8942ba1ee476df3eab5bd8661734a94cf186b492
Kasper Jacobsen <k@mackwerk.dk>
2014-09-23 13:37:45 +0200

    Add --diff-only

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 
 from isort import isort
 
@@ -7,11 +6,12 @@ from isort import isort
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to run')
+    parser.add_argument('--diff-only', action='store_true', dest='diff_only')
+    parser.set_defaults(diff_only=True)
     args = parser.parse_args(argv)
-    print args
-    # argv = argv if argv is not None else sys.argv[1:]
-    # for filename in argv:
-    #     isort.SortImports(filename, show_diff=True)
+
+    for filename in args.filenames:
+        isort.SortImports(filename, show_diff=args.diff_only)
     return 0
 
 if __name__ == '__main__':
