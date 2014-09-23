c951a76dae3fbee353414f04246ff74993e50978
Kasper Jacobsen <k@mackwerk.dk>
2014-09-23 13:43:22 +0200

    dont show diff by default, be aggresive, be, be aggresive!

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -7,12 +7,20 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to run')
     parser.add_argument('--diff-only', action='store_true', dest='diff_only')
-    parser.set_defaults(diff_only=True)
+    parser.add_argument('--dont-check', action='store_false', dest='check')
+    parser.set_defaults(diff_only=False)
+    parser.set_defaults(check=True)
     args = parser.parse_args(argv)
+
+    result = []
 
     for filename in args.filenames:
         isort.SortImports(filename, show_diff=args.diff_only)
-    return 0
+        if args.check:
+            f = isort.SortImports(filename, check=args.check)
+            if f.incorrectly_sorted is True:
+                result.append(filename)
+    return len(result)
 
 if __name__ == '__main__':
     exit(main())
