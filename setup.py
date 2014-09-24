9c413206040377439dca75ab656c9060a38ec7e2
Anthony Sottile <asottile@umich.edu>
2014-09-24 06:06:48 -0700

    Support more versions of python.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+import sys
 from setuptools import find_packages
 from setuptools import setup
+
+install_requires = ['isort']
+if sys.version_info < (2, 7):
+    install_requires.append('argparse')
 
 
 setup(
@@ -14,14 +19,18 @@ setup(
     platforms='linux',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
     packages=find_packages('.', exclude=('tests*', 'testing*')),
-    install_requires=[
-        'isort',
-    ],
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'python-import-sorter = pre_commit_hook.sort:main',
