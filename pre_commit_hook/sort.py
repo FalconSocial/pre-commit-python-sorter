9ab55686f50ca605abeb140176d03e7ee4f4e55f
Jonas Trappenberg <jonas@trappenberg.ch>
2015-12-01 13:32:02 -0800

    Add '--check-only' flag

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -1,30 +1,35 @@
 from __future__ import print_function
+
 import argparse
 import os
 
 from isort import isort
 
 
+def imports_incorrect(filename):
+    return isort.SortImports(filename, check=True).incorrectly_sorted
+
+
 def main(argv=None):
-    def imports_incorrect(filename):
-        return isort.SortImports(filename, check=True).incorrectly_sorted
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to run')
-    parser.add_argument('--silent-overwrite', action='store_true', dest='silent')
-    parser.set_defaults(silent=False)
+    parser.add_argument('--silent-overwrite', action='store_true', dest='silent', default=False)
+    parser.add_argument('--check-only', action='store_true', dest='check_only', default=False)
     args = parser.parse_args(argv)
 
     return_value = 0
 
     for filename in args.filenames:
-        if imports_incorrect(filename) is True:
-            if args.silent is False:
-                return_value =  1
+        if imports_incorrect(filename):
+            if args.check_only:
+                return_value = 1
+            elif args.silent:
+                isort.SortImports(filename)
+            else:
+                return_value = 1
                 isort.SortImports(filename)
                 print('FIXED: {0}'.format(os.path.abspath(filename)))
-            else:
-                isort.SortImports(filename)
     return return_value
 
 if __name__ == '__main__':
